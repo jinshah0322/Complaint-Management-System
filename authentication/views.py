cd379a824c7741bedfd1907e9b7531fa03ef5736
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2022-10-20 18:49:34 +0530

    Update views.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 # Create your views here.
 def home(request):
     return render(request,"authentication/index.html")
 
 def signup(request):
+    if request.method == "POST":
+        Name=request.POST['Name']
+        email=request.POST['email']
+        password=request.POST['password']
+        confirm_password=request.POST['confirm_password']
+        myuser=User.objects.create_user(Name, email, password)
+        myuser.save()
+        messages.success(request,"Your account has been successfully created")
+        return redirect('/signin')
     return render(request,"authentication/signup.html")
 
 def signin(request):
+    if(request.method=="POST"):
+        Name=request.POST['Name']
+        password=request.POST['Password']
+        user=authenticate(username=Name,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+        else:
+            messages.error(request,"Invalid Credentials")
+            return redirect('/signin')
     return render(request,"authentication/signin.html")
 
 def signout(request):
