1cf3a61c6b691d0e36f9c3ca30aa3e882f7e1ab7
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2022-10-21 00:55:26 +0530

    Added configuration for email service

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,20 @@
+from email.message import EmailMessage
+from tokenize import generate_tokens
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from cms import settings
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from cms.tokens import generate_token
+from django.core.mail import EmailMessage, send_mail
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
 # Create your views here.
 def home(request):
     return render(request,"authentication/index.html")
@@ -13,10 +25,57 @@ def signup(request):
         email=request.POST['email']
         password=request.POST['password']
         confirm_password=request.POST['confirm_password']
+
+        if User.objects.filter(username=Name):
+            messages.error(request,"Username already exists",extra_tags="validation")
+            return redirect('/signup')
+
+        if User.objects.filter(email=email):
+            messages.error(request,"Email already exists",extra_tags="validation")
+            return redirect('/signup')
+
+        if password != confirm_password:
+            messages.error(request,"Password does not match",extra_tags="validation")
+            return redirect('/signup')
+
+        if not Name.isalnum():
+            messages.error(request,"Username should only contain letters and numbers",extra_tags="validation")
+            return redirect('/signup')
+
         myuser=User.objects.create_user(Name, email, password)
+        myuser.first_name=Name
+        myuser.email=email
+        myuser.is_active = False
         myuser.save()
-        messages.success(request,"Your account has been successfully created")
-        return redirect('/signin')
+        messages.success(request,"Your account has been successfully created",extra_tags="valid")
+
+        #welcome email
+        subject = "Welcome to Complaint Management System"
+        message = "Hi "+myuser.first_name+"! Welcome to Complaint Management System!!!.\n We are glad to have you here!!!.\nWe have sent you a confirmation email to "+myuser.email+".\nPlease confirm your email to continue using our services.\n\nThank You!!!"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+
+        #confirmation email
+        current_site = get_current_site(request)
+        email_subject = "Confirm your email"
+        message2 = render_to_string('email_confirmation.html', {
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+        })
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email]
+        )
+        email.fail_silently = True
+        email.send()
+
+        return redirect('/')
+
     return render(request,"authentication/signup.html")
 
 def signin(request):
@@ -26,11 +85,28 @@ def signin(request):
         user=authenticate(username=Name,password=password)
         if user is not None:
             login(request,user)
-            return redirect('/')
+            return redirect('/home')
         else:
-            messages.info(request,"Invalid Credentials")
-            return redirect('/signin')
+            messages.error(request,"Invalid Credentials",extra_tags="invalid")
+            return redirect('/')
     return render(request,"authentication/signin.html")
 
 def signout(request):
-    pass
+    logout(request)
+    messages.success(request,"You have been successfully logged out",extra_tags="logout")
+    return redirect('/')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('/home')
+    else:
+        return render(request, 'activation_failed.html')
