3ced9be8e0d0a09e8b96df35e66d658bfbb1bd7f
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2022-10-21 00:56:04 +0530

    Added email config

--- cms/settings.py
+++ cms/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from cms.info import *
+
+EMAIL_USE_TLS = EMAIL_USE_TLS
+EMAIL_HOST = EMAIL_HOST
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_PORT = EMAIL_PORT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
