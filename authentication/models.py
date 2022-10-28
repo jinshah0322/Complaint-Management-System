a26ecfac8762746f84ba5a81517a3bbb952fb6be
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2022-10-28 12:56:22 +0530

    added __str__ function

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,6 @@
-from email.policy import default
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import User
+from cms import settings
 
 # Create your models here.
 class Complaint(models.Model):
@@ -9,9 +10,15 @@ class Complaint(models.Model):
     priority = models.CharField(max_length=10,choices=CHOICES)
     date = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return self.complaint_name,self.complaint_description,self.priority,self.date
+
 class Contactus(models.Model):
     name = models.CharField(max_length=100,null=True)
     phoneNumber = models.CharField(max_length=10,null=True)
     email = models.EmailField(max_length = 254,null=True)
     message = models.TextField(null=True)
     date_of_register = models.DateField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.name,self.phoneNumber,self.email,self.message,self.date_of_register
